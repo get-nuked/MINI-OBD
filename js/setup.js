9b2320639fc845616b24d99db4ccffebaa3778c8
get-nuked <vinuk.guna@gmail.com>
2026-08-14 17:18:24 +0100

    added a directory picker

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,9 @@
 const connectButton = document.getElementById("connectButton")
 const status = document.getElementById("status")
+const folderButton = document.getElementById("folderButton")
+const folderStatus =  document.getElementById("folderStatus")
 
+let logDirectory = null
 let port
 let logFile = null
 
@@ -8,8 +11,23 @@ import { writeToLogFile } from "./logger.js"
 import { saveLogFileHandle } from "./storage.js"
 
 
+folderButton.addEventListener("click", async () => {
+    try {
+        logDirectory = await window.showDirectoryPicker({ mode: "readwrite" })
+        folderStatus.textContent = "Log folder: " + logDirectory.name
+    } catch (error) {
+        console.error(error)
+        folderStatus.textContent = "No log folder selected"
+    }
+})
+
+
 connectButton.addEventListener("click", async () => {
     try { // Check that the browser supports Web Serial
+        if(!logDirectory) {
+            throw new Error("Please select a log folder before connecting to the OBD adapter.")
+        }
+
         if (!("serial" in navigator)) {
             throw new Error(
                 "Web Serial is not supported by this browser."
@@ -22,13 +40,12 @@ connectButton.addEventListener("click", async () => {
         port = await navigator.serial.requestPort()
 
         // Opens the serial connection to the adapter
-        await port.open({
-            baudRate: 38400
-        })
+        await port.open({baudRate: 38400})
 
         status.textContent = "Connected"
 
-        await createsessionLogFile()
+        const logFile = await createsessionLogFile()
+        await saveLogFileHandle(logFile)
         await port.close() // Close the port after creating the log file
         window.location.href = "cmd.html";
 
@@ -41,24 +58,8 @@ connectButton.addEventListener("click", async () => {
 
 async function createsessionLogFile() {
     const now = new Date()
-
     const filename = `MINI-OBD-${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}-${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}.txt`
-
-    try {
-        logFile = await window.showSaveFilePicker({
-            suggestedName: filename,
-            types: [
-                {
-                    description: "Text Files",
-                    accept: { "text/plain": [".txt"] }
-                }
-            ]
-        })
-        await saveLogFileHandle(logFile)
-    } catch (error) {
-        console.error(error)
-        status.textContent += "\nERROR: " + error.message
-    }
+    const logFile = await logDirectory.getFileHandle(filename, { create: true })  
 
     await writeToLogFile(logFile, "MINI OBD Session Log\n" + "Session started at: " + now.toLocaleString() + "\n\n")
 }
